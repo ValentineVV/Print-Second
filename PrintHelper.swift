c1777a70c695666a034ad51ab98670d47976c013
Valiantsin Vasiliavitski <valiantsin_vasiliavitski@epam.com>
2020-12-12 15:51:54 +0300

    added print func

---
+++ PrintHelper.swift
@@ -0,0 +1,12 @@
+//
+//  PrintHelper.swift
+//  Print_Second
+//
+//  Created by Valiantsin Vasiliavitski on 12/12/20.
+//
+
+import Foundation
+
+func print(text: String = "Second") {
+    print(text)
+}
